8a90206e798b7b734a9267b1c9d17612fe712cb8
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2022-10-11 09:59:57 +0000

    WIP

--- app/components/discussion_header_component.rb
+++ app/components/discussion_header_component.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
-class DiscussionFormComponent < ViewComponent::Base
+class DiscussionHeaderComponent < ViewComponent::Base
   include Turbo::FramesHelper
-  
+
   def initialize(discussion:)
     @discussion = discussion
   end
-
 end
